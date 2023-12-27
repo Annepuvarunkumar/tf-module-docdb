2964a5b762ee933311c238bf670255e40af3c38d
varun <varunannepu123@gmail.com>
2023-12-27 21:04:04 +0530

    docdb cluster

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "aws_ssm_parameter" "foo" {
   name = "docdb.${var.env}.master_username"
 }
 
-data "aws_ssm_parameter" "foo" {
+data "aws_ssm_parameter" "main" {
   name = "docdb.${var.env}.master_password"
 }
 
